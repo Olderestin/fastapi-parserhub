ccce1aeba64ceeae316cdef0540279ef9b8e960c
Olderestin <sdordiuk@gmail.com>
2024-01-18 02:25:30 +0200

    Refactor import

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
-from .config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 Base: DeclarativeMeta = declarative_base()
